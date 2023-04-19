6b71a1a39a14a3b13b64344d9bac97cd354e73b2
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-04-19 16:08:43 +0900

    Police AI - 01

--- Assets/Scripts/PoliceAI.cs
+++ Assets/Scripts/PoliceAI.cs
@@ -1,7 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PoliceAI : MonoBehaviour {
-    
+    [SerializeField] private Transform target;
+    [SerializeField] private NavMeshPath navMeshPath;
+
+    private float pathUpdateTime;
+
+    private void Init() {
+        this.navMeshPath = new NavMeshPath();
+        this.pathUpdateTime = 0.0f;
+    }
+
+    private void Awake() {
+        Init();
+    }
+
+    private void Update() {
+        Pathing();
+    }
+
+    private void FixedUpdate() {
+        Chasing();
+    }
+
+    private void Pathing() {
+        this.pathUpdateTime += Time.deltaTime;
+
+        if (this.pathUpdateTime > 0.1f) {
+            this.pathUpdateTime = 0.0f;
+            NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, this.navMeshPath);
+        }
+    }
+
+    private void Chasing() {
+        
+    }
 }
